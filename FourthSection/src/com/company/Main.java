5848c58e1aca451c67a3f7219b3ee4a5c0b6fd3f
vasus1998 <79025123+vasus1998@users.noreply.github.com>
2022-04-01 15:49:48 +0300

    Refactoring

--- FourthSection/src/com/company/Main.java
+++ FourthSection/src/com/company/Main.java
@@ -4,17 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static int first() {
-        System.out.print("Input number: ");
-        int number = scanner.nextInt();
+    public static int first(int number) {
         return Integer.compare(number, 0);
     }
 
-    public static int second() {
-        System.out.print("Input number: ");
-        int number = scanner.nextInt();
+    public static int second(int number) {
         int counter = 0;
         for (int i = number; i < (2 * number); i++) {
             for (int j = 1; j < i; j++) {
@@ -26,12 +21,7 @@ public class Main {
         return counter;
     }
 
-    public static int third() {
-        System.out.print("Input number X: ");
-        int theXNumber = scanner.nextInt();
-        System.out.println();
-        System.out.print("Input D number: ");
-        int theDNumber = scanner.nextInt();
+    public static int third(int theXNumber, int theDNumber) {
         int counter = 0;
         while (theXNumber > 0) {
             if ((theXNumber % 10) == theDNumber) {
@@ -42,9 +32,7 @@ public class Main {
         return counter;
     }
 
-    public static int fourth() {
-        System.out.print("Input number: ");
-        int number = scanner.nextInt();
+    public static int fourth(int number) {
         int firstCounter = 0; // четные
         int secondCounter = 0; // нечетные
         while (number > 0) {
@@ -58,23 +46,11 @@ public class Main {
         return Integer.compare(firstCounter, secondCounter);
     }
 
-    public static double fifth() {
-        System.out.print("A(x) = ");
-        int ax = scanner.nextInt();
-        System.out.print("A(y) = ");
-        int ay = scanner.nextInt();
-        System.out.print("B(x) = ");
-        int bx = scanner.nextInt();
-        System.out.print("B(y) = ");
-        int by = scanner.nextInt();
+    public static double fifth(int ax, int ay, int bx, int by) {
         return Math.sqrt(Math.pow((ax - bx), 2) + Math.pow((ay - by), 2));
     }
 
-    public static int sixth() {
-        System.out.print("A(x) = ");
-        int ax = scanner.nextInt();
-        System.out.print("A(y) = ");
-        int ay = scanner.nextInt();
+    public static int sixth(int ax, int ay) {
         if (ax > 0 && ay > 0) return 1;
         if (ax < 0 && ay > 0) return 2;
         if (ax < 0 && ay < 0) return 3;
@@ -82,29 +58,19 @@ public class Main {
         return -1;
     }
 
-    public static String seventh() {
-        System.out.print("Координаты центра круга: A(x) = ");
-        int ax = scanner.nextInt();
-        System.out.print("A(y) = ");
-        int ay = scanner.nextInt();
-        System.out.print("Координаты точки B(x) = ");
-        int bx = scanner.nextInt();
-        System.out.print("A(y) = ");
-        int by = scanner.nextInt();
-        System.out.print("Radius = ");
-        int radius = scanner.nextInt();
+    public static String seventh(int ax, int ay, int bx, int by, int radius) {
         double length = Math.sqrt(Math.pow((ax - bx), 2) + Math.pow((ay - by), 2));
         return (length > radius) ? "Точка вне круга" : "Точка в круге";
     }
 
     public static void main(String[] args) {
-        System.out.println(Main.first());
-        System.out.println(Main.second());
-        System.out.println(Main.third());
-        System.out.println(Main.fourth());
-        System.out.println(Main.fifth());
-        System.out.println(Main.sixth());
-        System.out.println(Main.seventh());
+        System.out.println(Main.first(-66));
+        System.out.println(Main.second(34));
+        System.out.println(Main.third(23, 2));
+        System.out.println(Main.fourth(12));
+        System.out.println(Main.fifth(12,-15, 24, 12));
+        System.out.println(Main.sixth(33, 332));
+        System.out.println(Main.seventh(33,33,33,33,44));
     }
 
 }
